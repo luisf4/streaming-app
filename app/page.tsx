987419e0bf804aea63929de400f6bd780f289e83
luisf4 <luismarcelino.dev@gmail.com>
2024-01-16 12:05:56 -0300

    feat: implemented authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <Button size={'sm'}>Thing</Button>
+    <div className='flex flex-col gap-y-4'>
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl='/'/>
+    </div>
   )
 }
