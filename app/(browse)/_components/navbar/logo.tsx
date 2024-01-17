eb94d74f189f319d4ffc8d18399bbac307d938f5
luisf4 <luismarcelino.dev@gmail.com>
2024-01-17 14:17:31 -0300

    feat: implemented navbar

---
+++ app/(browse)/_components/navbar/logo.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import { Poppins } from "next/font/google"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["200", "300", "400", "500", "600", "700", "800", "900"]
+})
+
+export const Logo = () => {
+    return (
+        <Link href="/">
+            <div className="hidden lg:flex items-center gap-x-4 hover:opacity-80 transition">
+                <div className="bg-white rounded-full p-1">
+                    <Image
+                        src="/spooky.svg"
+                        alt="Logo" width={60}
+                        height={60}
+                    />
+                </div>
+                <div className={cn(font.className)}>
+                    <p className="text-lg font-semibold">
+                        Gamehub
+                    </p>
+                    <p className="text-xs text-muted-foreground">
+                        Let&apos;s play
+                    </p>
+                </div>
+            </div>
+        </Link>
+    )
+}
