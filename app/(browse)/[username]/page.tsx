5d2e02b45776754172c441ce8b79c79380daa35a
luisf4 <luismarcelino.dev@gmail.com>
2024-01-22 09:59:17 -0300

    feat: implemented block users

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,7 @@ import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-services";
 import { notFound } from "next/navigation";
 import { Actions } from "./_components/actions";
+import { isBlockedByUser } from "@/lib/block-service";
 
 interface UserPageProps {
   params: {
@@ -16,13 +17,19 @@ const UserPage = async ({ params }: UserPageProps) => {
     notFound();
   }
   const isFollowing = await isFollowingUser(user.id);
+  const isBlocked = await isBlockedByUser(user.id);
+
+  if (isBlocked) {
+    notFound();
+  }
 
   return (
     <div>
-      <p>Username: {user?.username}</p> 
+      <p>Username: {user?.username}</p>
       <p>User id: {user.id}</p>
       <p>Following: {`${isFollowing}`}</p>
-      <Actions userId={user.id} isFollowing={isFollowing}/>
+      <p>Is blocked by this user: {`${isBlocked}`}</p>
+      <Actions userId={user.id} isFollowing={isFollowing} />
     </div>
   );
 };
